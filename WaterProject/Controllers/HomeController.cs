69549c55b70d9ad9c472c5d8e19641f5394eaf6d
janae <ddlion15@byu.edu>
2022-02-16 15:44:17 -0700

    Mission7 Commit

---
+++ WaterProject/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WaterProject.Models;
+using WaterProject.Models.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WaterProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IWaterProjectRepository repo;
+
+        public HomeController (IWaterProjectRepository temp)
+        {
+            repo = temp;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 5;
+
+            var x = new ProjectsViewModel
+            {
+                Projects = repo.Projects
+                .OrderBy(p => p.ProjectName)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumProjects = repo.Projects.Count(),
+                    ProjectsPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+
+            return View(x);
+        }
+
+    }
+}
---
+++ WaterProject/Models/ViewModels/PageInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace WaterProject.Models.ViewModels
+{
+    public class PageInfo
+    {
+        public int TotalNumProjects { get; set; }
+
+        public int ProjectsPerPage { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        public int TotalPages => (int) Math.Ceiling((double) TotalNumProjects / ProjectsPerPage);
+
+    }
+}
